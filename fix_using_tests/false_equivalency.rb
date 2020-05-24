8703234d22ca10456a768e87a42925c8a2886b8a
Larissa Nguyen <larissa.nguyen@yale.edu>
2020-05-24 05:47:05 +0000

    Done.

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,7 +1,9 @@
 # don't forget to add: require 'pry'
+require 'pry'
 
 def get_user_input
   gets.chomp
+  # binding.pry
 end
 
 def prompt_user
@@ -11,9 +13,9 @@ def prompt_user
 end
 
 def selection(num)
-  if num = 1
+  if num == 1
      "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
+  elsif num == 2
      "HAM HAM HAM IN MY TUMMY"
   end
 end
@@ -21,4 +23,5 @@ end
 def runner
   prompt_user
   selection(get_user_input)
+  # binding.pry
 end
--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,16 @@
 # don't forget to add: require 'pry'
+require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
+    num = 0 
+    while num < 10
+      string = "s" + string
+      num += 1
+    end
   else
     string
   end
+  
+  return string
 end
